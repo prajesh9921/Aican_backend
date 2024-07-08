26453bd6c52ecaf91c6407da543249f8411eb40e
prajesh9921 <prajeshgawhale2019.it@mmcoe.edu.in>
2024-07-08 19:48:11 +0530

    added project

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require('http');
+const { Server } = require('socket.io');
+const dotenv = require("dotenv");
+const fs = require('fs');  // Add this line to include the fs module
+
+// Initialize app and server
+dotenv.config();
 const app = express();
-var cors = require("cors");
-const port = 3500;
-const allRoutes  = require("./routes/allRoutes");
+const server = http.createServer(app);
 
 // Middleware
-const dotenv = require("dotenv");
-const errorHandler = require("./middleware/errorHandler");
-dotenv.config();
-
 app.use(cors());
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("connection established to MongoDB");
-  })
-  .catch((err) => {
-    console.log("error connecting to MongoDB", err);
-  });
-
-const PORT = process.env.PORT || port;
-const HOST = process.env.HOST || "localhost";
-
-app.get("/", async (req, res) => {
-  res.json({message: "Default Route"});
-});
-
 app.use(express.json());
 
-app.use("/api", allRoutes);
+const io = new Server(server, {
+  cors: {
+    origin: 'https://heyhomie.netlify.app',
+    methods: ['GET', 'POST']
+  }
+});
 
+const PORT = process.env.PORT || 3500;
+const HOST = process.env.HOST || "localhost";
+
+// Event listener for socket connections
+io.on('connection', (socket) => {
+  socket.on('text', (text) => {
+    fs.writeFile('savedText.txt', text, (err) => {
+      if (err) {
+        console.error('Error writing to file', err);
+      }
+    });
+    // Broadcast the text to other clients
+    socket.broadcast.emit('gettext', text);
+  }); 
+});
+
+// Endpoint to retrieve saved text
+app.get("/gettext", async (req, res) => {
+  fs.readFile('savedText.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file', err);
+      res.status(500).json({ message: "Error reading file" });
+    } else {
+      res.status(200).json({ text: data });
+    }
+  });
+});
+
+// Default route
+app.get("/", async (req, res) => {
+  res.json({ message: "Default Route" });
+});
+
+// Handle undefined routes
 app.use("/*", (req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port https://${HOST}:${PORT}`);
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
 });
